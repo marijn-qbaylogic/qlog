f57c7f13fc0f0799aac8e67f51f28398af32963b
Marijn <marijn@qbaylogic.com>
2026-06-23 16:19:25 +0200

    Wrote a conversion script for old entries

---
+++ convert.py
@@ -0,0 +1,77 @@
+# simple tool for converting old changelog files
+
+import sys
+import re
+import os
+import argparse
+
+parser = argparse.ArgumentParser(prog="convert.py", description="Convert old changelog entries to new format.", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-s","--save", action="store_true", dest="save", help="Save converted entries to ./changelog/entries")
+parser.add_argument("-d","--delete", action="store_true", dest="delete", help="Delete old entries")
+parser.add_argument("path", help="File or folder of files to convert")
+
+args = parser.parse_args()
+
+SAVE = args.save
+DELETE = args.delete
+todo = args.path
+
+
+
+WHITE="\033[97m"
+RED="\033[91m"
+GREEN="\033[92m"
+BLACK="\033[90m"
+YELLOW="\033[93m"
+
+if os.path.isdir(todo):
+    files = [os.path.join(todo,x) for x in os.listdir(todo) if not x=="README.md" and not x.endswith(".py")]
+else:
+    files = [todo]
+
+for file in files:
+    print(BLACK+"-"*150)
+    print(file)
+    with open(file,"r") as fp:
+        text = fp.read()
+    print(RED+text.strip())
+
+    if m:=re.match(r"([A-Z]+):\s*(.*)",text,re.DOTALL):
+        cat,text = m.groups()
+    else:
+        cat = None
+    
+    if m:=re.match("(.*?)( See| Fixes)? \[#(\d+)\]\(.*\)\.?",text,re.DOTALL):
+        text,_,issue = m.groups()
+    else:
+        issue = None
+    
+    text=text.strip()
+
+    print(f"""{WHITE}Category: {YELLOW}{cat}
+{WHITE}Issue: {YELLOW}{issue}
+{WHITE}Content:
+{YELLOW}{text.strip()}
+""")
+
+    if not issue: issue = ""
+
+    output = f"""---
+issues: [{issue}]
+---
+
+# {cat}
+
+{text}
+"""
+
+    print(GREEN+output)
+    
+    if DELETE:
+        os.remove(file)
+
+    if SAVE:
+        with open(os.path.join("changelog/entries",os.path.basename(file)),"w") as fp:
+            fp.write(output)
+
+
